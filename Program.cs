b8c1e1d03649b0b37a32326d48b825e88f2d135f
cabyrc2910 <cabyrc@list.ru>
2022-09-20 23:09:08 +0300

    add task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых  меньше либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не пользоваться коллекциями, обойтись исключительно массивами.
+// Примеры: 
+// ["hello","2","World",":-)"]->["2",":-)"]
+// ["1234","1567,"-2","computer science"]->["-2"]
+// ["Russia","Denmark","Kazan"]->[]
+
+Console.Clear();
+string[] str = new string[] { "47", "Пермь", "Петербург", "Волгоград", "777", "Астрахань" };
+Console.WriteLine("Исходный массив: "  + string.Join(" ", str));
+int count = 0;
+for (int i = 0; i < str.Length; i++) 
+{
+    if (str[i].Length <= 3) count++;
+}
+
+int newStrCounter = 0;
+
+string[] newStr = new string[count];
+for (int i = 0; i < str.Length; i++)
+{
+    if (str[i].Length <= 3) {
+
+        newStr[newStrCounter] = str[i];
+        newStrCounter++;
+
+    };
+}
+Console.Write("Новый массив: ");
+for (int i = 0; i < newStr.Length; i++) 
+{
+    Console.Write(newStr[i] + " ");
+}
